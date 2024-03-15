66847b6941db42c523e8215d4bea751f03dd56ac
co <corentin.bel@protonmail.com>
2024-03-15 22:19:38 +0100

    Added messages

--- code/agent.py
+++ code/agent.py
@@ -2,9 +2,29 @@
 Agent class for the anthropic agent
 """
 
+import anthropic
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import Any, List, Optional
 
 class AnthropicAgent(BaseModel):
-    name: str
-    style: str
+    # Setting defaults for the agent
+    name: str = "No Name"
+    style: str = "Normal Style"
+    client: anthropic.Anthropic = anthropic.Anthropic()
+
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    # Defining a ask function, which queries the Anthropic API
+    def ask(self, message: str) -> str:
+
+        with self.client.messages.stream(
+            max_tokens=1024,
+            messages=[{"role": "user", "content": message}],
+            model="claude-3-opus-20240229",
+        ) as stream:
+            for text in stream.text_stream:
+                print(text, end="", flush=True)
+        return text
+        
